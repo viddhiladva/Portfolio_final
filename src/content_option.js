e091c1c3b49a9807cedb4fd66d6bc0514e9f771b
Viddhi Ladva <vidhiladva@gmail.com>
2024-11-10 19:33:21 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,180 @@
+const logotext = "VIDDHI";
+const meta = {
+title: "Viddhi Ladva",
+    description: "I’m Viddhi Ladva Full stack Web developer @ Logwintech PVT. LTD.",
+};
+
+const introdata = {
+    title: "I’m Viddhi Ladva",
+    animated: {
+        first: "I love coding.",
+        second: "I code cool websites.",
+        third: "I develop, that leads to success.",
+    },
+    description: "Hello there, I'm Full-stack web developer passionated about building dynamic full-stack web applications that leads to the success and growth of the product.",
+    // my image in the front page of the website
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "I'm a passionate web developer skilled in creating websites and web applications. I’m always eager to contribute, learn, and collaborate. If you have an opportunity that matches my skills, feel free to reach out!"
+};
+
+const worktimeline = [{
+        jobtitle: "B.Tech",
+        where: "Sarvjanik College of Engineering and Technology",
+        date: "2024",
+    },
+    {
+        jobtitle: "Tech Lead ( Web Dept )",
+        where: "S3CloudHub",
+        date: "2021-2024",
+    },
+    {
+        jobtitle: "Software Engineer",
+        where: "LogWinTech Pvt. Ltd.",
+        date: "2024-current",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML/CSS",
+        value: 95,
+    },
+    {
+        name: "JavaScript",
+        value: 85,
+    },
+    {
+        name: "TypeScript",
+        value: 60,
+    },
+    {
+        name: "React/Next.js",
+        value: 80,
+    },
+    {
+        name: "Node.js/Express.js",
+        value: 50,
+    },
+    {
+        name: "MongoDB",
+        value: 50,
+    },
+];
+
+
+const services = [
+    {
+        title: "UI & UX Design",
+        description: "Crafting intuitive and engaging user interfaces with a focus on user experience to enhance usability and satisfaction."
+    },
+    {
+        title: "Web Design",
+        description: "Creating visually appealing and responsive web designs that align with brand identity and ensure a seamless user experience."
+    },
+    {
+        title: "Web Development",
+        description: "Developing robust and scalable web applications using modern frameworks and technologies to meet diverse client needs."
+    },
+    {
+        title: "AWS Solutions",
+        description: "Providing comprehensive cloud solutions, including deployment, management, and optimization of infrastructure using AWS services."
+    }
+];
+
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "viddhi.work@gmail.com",
+    YOUR_FONE: "(+91) 78020 99520",
+    description: "Feel free to Contact me by submitting the form, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_4sqwcua",
+    YOUR_TEMPLATE_ID: "template_sknpya5",
+    YOUR_USER_ID: "jQi5ifW2X2Ky0vot0",
+};
+
+const socialprofils = {
+    github: "https://github.com/viddhiladva",
+    linkedin: "https://www.linkedin.com/in/viddhi-ladva-b20144267/",
+    twitter: "https://x.com/ViddhiLadva",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
